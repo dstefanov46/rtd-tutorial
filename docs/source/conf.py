de4f9a57697a05031b0275433dcef70d04bee36b
jojomale <33347198+jojomale@users.noreply.github.com>
2021-10-28 16:17:21 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
-
 
 project = 'Lumache'
 copyright = '2021, Graziella'
